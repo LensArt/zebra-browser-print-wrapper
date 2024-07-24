357d361a738dc0f8e339f37b0420841679773cce
Cody Auch <cody.auch@nmlgc.onmicrosoft.com>
2024-07-24 14:19:10 -0500

    Add transpiled js files

---
+++ lib/index.d.ts
@@ -0,0 +1,18 @@
+import { Device } from './types';
+export default class ZebraBrowserPrintWrapper {
+    device: Device;
+    API_URL: string;
+    constructor(api_url: string);
+    getAvailablePrinters: () => Promise<any>;
+    getDefaultPrinter: () => Promise<Device>;
+    setPrinter: (device: Device) => void;
+    getPrinter: () => Device;
+    cleanUpString: (str: string) => string;
+    checkPrinterStatus: () => Promise<{
+        isReadyToPrint: boolean;
+        errors: string;
+    }>;
+    write: (data: string) => Promise<void>;
+    read: () => Promise<string>;
+    print: (text: string) => Promise<void>;
+}
